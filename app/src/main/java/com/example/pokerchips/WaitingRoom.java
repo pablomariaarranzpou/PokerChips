b91bde04febb44311fae5b040cc0a93314bf012c
pablomariaarranzpou <1o233Hm3>
2022-06-26 22:06:33 +0200

    CONEXIÓN A FIREBASE

---
+++ app/src/main/java/com/example/pokerchips/WaitingRoom.java
@@ -0,0 +1,14 @@
+package com.example.pokerchips;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WaitingRoom extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
