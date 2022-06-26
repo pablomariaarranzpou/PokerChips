ba3b94eb310fabdad2ff9742992a04fa8f9241e1
pablomariaarranzpou <1o233Hm3>
2022-06-26 19:25:24 +0200

    View pantalla  1 y 2 de DEMO

--- app/src/main/java/com/example/pokerchips/MainActivity.java
+++ app/src/main/java/com/example/pokerchips/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.pokerchips;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_createRoom, btn_joinRoom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_createRoom = findViewById(R.id.create_menu);
+        btn_createRoom.setOnClickListener(v-> startActivity(new Intent(MainActivity.this, CreateRoom.class)));
     }
 }
